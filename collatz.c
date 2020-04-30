312686abf82fbdd4747b8f385c9d8ff658e52c02
cjones-32 <cjones0032@gmail.com>
2020-04-30 15:13:43 -0500

    Create collatz.c

---
+++ collatz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <cs50.h>
+
+void collatz(int number);
+
+//delcared here to prevent constant resetting by recursion
+int count = 0;
+
+//get a number from user and send to function collatz
+int main(void)
+{
+    collatz(get_int("Number: "));
+}
+
+//if a number is one, print count. if its even devide by 2 and add to count. if its odd, times by 3 then add one.
+void collatz(int number)
+{
+    if (number == 1)
+    {
+        printf("%i\n", count);
+        return;
+    }
+    else if (number % 2 == 0)
+    {
+        count++;
+        collatz(number / 2);
+    }
+    else 
+    {
+        count++;
+        collatz(number * 3 + 1);
+    }
+}
